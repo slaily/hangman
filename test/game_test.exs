20ea40ed66e3509f778b2f71ca094a059f73ddeb
Iliyan Slavov <slavov.iliyan96@gmail.com>
2018-12-21 10:51:58 +0200

    Added implementation of duplicating moves and score a good move for our game including tests.

--- test/game_test.exs
+++ test/game_test.exs
@@ -10,4 +10,44 @@ defmodule GameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [ :won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("wibble")
+    { game, _tally } = Game.make_move(game, "w")
+
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    game = Game.new_game("wibble")
+
+    for letter <- game.letters do
+      { game, _tally } = Game.make_move(game, letter)
+      assert game.game_state == :good_guess
+    end
+  end
 end
